2a4559304da840123c526c6616e2b1c73cbfb712
roro <83040482+roro9999@users.noreply.github.com>
2022-08-30 20:43:41 -0400

    k

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div className='ml-20'>
-              <p className='text-2xl font-semibold pt-6 ml-8'>Dashboard</p>
+    <div className='ml-28'>
+              <p className='text-2xl font-semibold pt-6'>Dashboard</p>
+
+              <div className='mt-6 bg-[#0a0e13] h-72 rounded-l-md'>
+                <p className='text-xl font-semibold ml-4 pt-4'>Preformace</p>
+                <p className='ml-4 mt-1 text-gray-600'>View recent checkouts and failures</p>
+
+                <div className='flex mt-7'>
+                  <div className='w-64 h-40 border border-[#1B1F25] bg-[#13181E] rounded-md ml-4'>
+
+                  </div>
+
+                  <div className='w-64 h-40 border border-[#1B1F25] bg-[#13181E] rounded-md ml-4'>
+
+                  </div>
+              </div>
+
+
+              </div>
       </div>
   )
 }
